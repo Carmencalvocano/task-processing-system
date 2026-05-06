336ac420a0dd5fef8309b812ac249b5ffb821c0e
Carmencalvocano <carmenchu.calvo@gmail.com>
2026-05-06 16:02:14 +0200

    Implement initial Task abstraction and matrix multiplication task

---
+++ core-cpp/build/CMakeFiles/task_system.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/Lenovo/OneDrive - UAB/Escritorio/AÑO 6/SEM 2/Proyectos/task-processing-system/core-cpp/src/MatrixMultiplyTask.cpp" "CMakeFiles/task_system.dir/src/MatrixMultiplyTask.cpp.o" "gcc" "CMakeFiles/task_system.dir/src/MatrixMultiplyTask.cpp.o.d"
+  "/mnt/c/Users/Lenovo/OneDrive - UAB/Escritorio/AÑO 6/SEM 2/Proyectos/task-processing-system/core-cpp/src/main.cpp" "CMakeFiles/task_system.dir/src/main.cpp.o" "gcc" "CMakeFiles/task_system.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ core-cpp/build/CMakeFiles/task_system.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task_system.dir/src/MatrixMultiplyTask.cpp.o"
+  "CMakeFiles/task_system.dir/src/MatrixMultiplyTask.cpp.o.d"
+  "CMakeFiles/task_system.dir/src/main.cpp.o"
+  "CMakeFiles/task_system.dir/src/main.cpp.o.d"
+  "task_system"
+  "task_system.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/task_system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
